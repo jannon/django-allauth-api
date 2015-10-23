4763bc5bcdae9251e8a693274ac3ae71870ed699
Jannon Frank <jannon@cs.stanford.edu>
2015-10-23 01:49:40 -0700

    remove unnecessary imports

--- src/allauth_api/account/rest_framework/adapter.py
+++ src/allauth_api/account/rest_framework/adapter.py
@@ -1,7 +1,5 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
-from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED
 from allauth.account import app_settings
 from allauth.account.adapter import USERNAME_REGEX
 from allauth.utils import get_user_model
